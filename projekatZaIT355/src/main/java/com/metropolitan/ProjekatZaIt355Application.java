cbb0b2e2caa323e9127a118ff4fab9fa43edd842
Jecika20 <jelena.radenkov@gmail.com>
2024-09-07 11:24:04 +0200

    first commit

---
+++ projekatZaIT355/src/main/java/com/metropolitan/ProjekatZaIt355Application.java
@@ -0,0 +1,13 @@
+package com.metropolitan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatZaIt355Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatZaIt355Application.class, args);
+	}
+
+}
---
+++ projekatZaIT355/src/test/java/com/metropolitan/ProjekatZaIt355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatZaIt355ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
