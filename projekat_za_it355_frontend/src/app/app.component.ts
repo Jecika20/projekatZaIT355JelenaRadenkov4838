f4f1d530d9d10af352149e22f7a0fd4bc73defe7
Jecika20 <jelena.radenkov@gmail.com>
2024-10-15 16:37:13 +0200

    started frontend

---
+++ projekat_za_it355_frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'projekat_za_it355_frontend';
+}
